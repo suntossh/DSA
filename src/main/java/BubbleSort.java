124d239035970b6be9a5558531a3e6d9c8f785d6
Santoshkumar pandey <suntossh@users.noreply.github.com>
2023-02-22 05:44:28 -0500

    Update BubbleSort.java

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -2,16 +2,14 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
-    static int[] unsortedArray = {1, 4, 6, 2, 0, 9, -3};
 
-    public static void swap(int i) {
+    public void swap(int[] unsortedArray, int i) {
         int temp = unsortedArray[i];
         unsortedArray[i] = unsortedArray[i + 1];
         unsortedArray[i + 1] = temp;
     }
 
-    public static void main(String[] args) {
-        Arrays.stream(unsortedArray).forEach(value -> System.out.print(value + " , "));
+    public int[] sort(int[] unsortedArray) {
         boolean isSwapCalled = true;
         int counter = 0;// since every while loop iteration is causing largest number to be switched to extreme right
         // so we need not loop for no reason till end of array all time.
@@ -19,13 +17,24 @@ public class BubbleSort {
             isSwapCalled = false;
             for (int i = 0; i < unsortedArray.length - 1 - counter; i++) {
                 if (unsortedArray[i] > unsortedArray[i + 1]) {
-                    swap(i);
+                    swap(unsortedArray, i);
                     isSwapCalled = true;
                 }
             }
             counter++;
         }
-        System.out.println("Sorted Array");
+        return unsortedArray;
+    }
+
+    public static void main(String[] args) {
+        int[] unsortedArray = {1, 4, 6, 2, 0, 9, -3};
+        System.out.print("Unsorted Array [] --> ");
         Arrays.stream(unsortedArray).forEach(value -> System.out.print(value + " , "));
+
+        BubbleSort bubbleSort = new BubbleSort();
+
+        int[] sortedArray = bubbleSort.sort(unsortedArray);
+        System.out.print("\n   Sorted Array[] --> ");
+        Arrays.stream(sortedArray).forEach(value -> System.out.print(value + " , "));
     }
 }
